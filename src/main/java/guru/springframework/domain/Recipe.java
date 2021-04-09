4952059367de0a1238b41e7cb5e8b0604befe5b3
XIAO <jinxiao.shawn@gmail.com>
2021-04-09 14:09:44 +0800

    Add ingredient entity(one to many)

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * @author XIAO JIN
@@ -22,6 +23,9 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
