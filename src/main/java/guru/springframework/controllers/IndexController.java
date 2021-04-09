2bcf148673816b5567d8c1d1b899ffe32929e283
XIAO <jinxiao.shawn@gmail.com>
2021-04-09 17:29:17 +0800

    Add spring data query method

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,31 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/index", "index.html"})
     public String getIndexPage() {
+
+        LOGGER.info("category ID : {}", categoryRepository.findByDescription("American").get().getId());
+        LOGGER.info("UOM ID : {}", unitOfMeasureRepository.findByDescription("Teaspoon").get().getId());
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,9 +3,12 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * @author XIAO JIN
  * @date 2021/04/09/ 16:19
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
